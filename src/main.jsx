22139bc45ef130c754d3f9e4a54faea25f965b23
Đình Nam <namndwebdev@gmail.com>
2023-08-31 11:27:52 +0700

    B1 Create template

--- src/main.jsx
+++ src/main.jsx
@@ -3,20 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-function Demo(){
-  var [x, setX] = useState("")
-  function onChange(e){
-    setX(e.target.value)
-  }
-
-  return <>
-    <input value={x} onChange={onChange}/>
-    <input value={x} onChange={onChange}/>
-  </>
-}
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Demo/>
+    <App/>
   </React.StrictMode>,
 )
