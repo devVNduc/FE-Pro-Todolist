605a4b4a1473d8fce2268a05133f1d89c67640fe
Đình Nam <namndwebdev@gmail.com>
2023-10-06 10:21:33 +0700

    B81 interceptor gan token moi request

--- src/config/axios.js
+++ src/config/axios.js
@@ -4,6 +4,10 @@ axios.defaults.baseURL = 'https://backoffice.nodemy.vn/api'
 axios.defaults.headers['Content-Type'] = 'application/json'
 axios.interceptors.request.use(config => {
     let token = store.getState().auth.token
-    config.headers['Authorization'] = `Bearer ${token}`
+    let bearerToken = config.headers.Authorization
+    let hasToken = bearerToken && bearerToken.split('Bearer')[1]
+    if(!hasToken){
+        config.headers['Authorization'] = `Bearer ${token}`
+    }
     return config
 }) 
